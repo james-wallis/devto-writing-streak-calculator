864c9de845cf73037a1a8217303d9f269c1af352
James Wallis <j@wallis.dev>
2021-04-13 23:52:45 +0100

    Writing -> Posting
    Signed-off-by: James Wallis <j@wallis.dev>

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,12 +33,12 @@ const IndexPage = () => {
   return (
     <div className="w-screen min-h-screen	flex items-center flex-col px-5 bg-body">
       <Head>
-        <title>Dev.to Writing Streak Calculator</title>
-        <meta name="description" content="Tool to calculate your current Dev.to Writing Streak. Are you on track for the next badge?" />
+        <title>Dev.to Posting Streak Calculator</title>
+        <meta name="description" content="Tool to calculate your current Dev.to Posting Streak. Are you on track for the next badge?" />
       </Head>
       <Header user={user} />
       <div className="flex flex-grow flex-col w-full">
-        <h1 className="text-3xl md:text-4xl font-semibold mt-6 md:mt-24 text-center">Dev.to Writing Streak Calculator (unofficial)</h1>
+        <h1 className="text-3xl md:text-4xl font-semibold mt-6 md:mt-24 text-center">Dev.to Posting Streak Calculator (unofficial)</h1>
         <span className="text-gray-500 text-sm mt-1 md:mt-2 text-center">All calculations are based on my experience. Read the Dev.to post for more information.</span>
         <div className="container py-6 md:py-24 mx-auto flex flex-wrap items-center justify-between max-w-site">
           <div className="lg:w-1/3 md:w-1/2 w-full bg-white rounded-lg p-6 md:p-8 flex flex-col w-full">
